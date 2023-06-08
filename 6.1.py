0f03466586a4c84ba4efe445931de1bf23011401
Kolorama <artemonzakirov2004@gmail.com>
2023-06-08 21:09:48 +0300

    Initial commit

---
+++ 6.1.py
@@ -0,0 +1,10 @@
+countries = {
+    "russia" : "moscow",
+    "france" : "paris",
+    "japan" : "tokyo",
+    "ukraine" : "kiev"
+}
+print("a", countries)
+x = input("Ввести страну")
+print("b", countries[x])
+print('c', sorted(countries))
